87ed74ca3c14f32bdb287c3e0784b5186ae25b87
devopsrudr <devopsrudra@gmail.com>
2024-03-11 01:21:53 -0400

    message

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,20 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+//Now, we will use for loop and two array methods: map method and forEach method to traverse through these arrays and display the menu items on the HTML page.
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
